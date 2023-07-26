c41ba34e732ce618f2d6609a51fe5194d6211218
almahmudsarker <almahmuds427@gmail.com>
2023-07-26 22:49:46 +0600

    6th commit

---
+++ convertRomanNum.js
@@ -0,0 +1,25 @@
+function romanToInt(romanNumeral) {
+  const romanValues = {
+    I: 1,
+    V: 5,
+    X: 10,
+    L: 50,
+    C: 100,
+    D: 500,
+    M: 1000,
+  };
+  let result = 0;
+  for (let i = 0; i < romanNumeral.length; i++) {
+    const currentChar = romanNumeral[i];
+    const nextChar = romanNumeral[i + 1];
+    if (romanValues[currentChar] < romanValues[nextChar]) {
+      result -= romanValues[currentChar];
+    } else {
+      result += romanValues[currentChar];
+    }
+  }
+  return result;
+}
+const romanNumeral = "XXI";
+const integerVal = romanToInt(romanNumeral);
+console.log(integerVal);
